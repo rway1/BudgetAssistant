ae38e5073b28b0e12fab1f9fc84921a609140432
Ryan K Way <ryan.keith.way@gmail.com>
2018-08-19 00:11:11 -0700

    Add WPF and Unit Test projects, write first unit tests

---
+++ BudgetAssistant/BudgetAssistantTest/CSVManagerTest.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BudgetAssistant;
+using System.IO;
+
+namespace BudgetAssistantTest
+{
+    [TestClass]
+    public class CSVManagerTest
+    {
+        static CSVManager csvManager = new CSVManager();
+
+        public void FileTesterHelper(string fileName)
+        {
+            Assert.IsTrue(File.Exists(fileName));
+            bool bCanBeOpened = false;
+            using (FileStream fileStream = new FileStream(fileName, FileMode.Open))
+            {
+                bCanBeOpened = true;
+            }
+            Assert.IsTrue(bCanBeOpened);
+        }
+
+        [TestMethod]
+        public void OpenDeductionsFileTest()
+        {
+            FileTesterHelper(CSVManager.PATH_TO_DEDUCTIONS_FILE);
+        }
+
+        [TestMethod]
+        public void OpenTransactionsFileTest()
+        {
+            FileTesterHelper(CSVManager.PATH_TO_TRANSACTIONS_FILE);
+        }
+    }
+}
---
+++ BudgetAssistant/BudgetAssistantTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("BudgetAssistantTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("BudgetAssistantTest")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("271d055c-f305-4e74-b0ff-5127f8f3c303")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
