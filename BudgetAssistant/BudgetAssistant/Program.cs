fef691767d4bb2e5aff72128871c2039f6faed37
Ryan K Way <ryan.keith.way@gmail.com>
2018-08-18 22:45:59 -0700

    Init commit

---
+++ BudgetAssistant/BudgetAssistant/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BudgetAssistant
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
